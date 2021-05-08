53b3254c417cc50fc71bcc320f55374157a71190
PabloD97 <pablo97758@gmail.com>
2021-05-07 21:02:38 -0300

    chore - Se agrego la dependencia de express - Se agrego la dependencia de desarrollo nodemon - Se agrego la dependencia de pug para el manejo de las vistas

---
+++ index.js
@@ -0,0 +1,22 @@
+// Version CommonJS
+// const express = require('express');
+
+import express from 'express';
+import router from './routes/index.js'; // en la nueva version de Node tenemos que colocar la extension del archivo que deseamos importa
+
+
+const app = express();
+
+// Definir puerto
+const port = process.env.PORT || 4000;
+
+    // Habilitar PUG
+    app.set('view engine','pug')
+
+// Agregar Router
+
+app.use('/', router);
+
+app.listen(port, ()=>{
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+})
