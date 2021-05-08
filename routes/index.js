7fca188658979ea7f9c942a71dcb9766dd147bdb
PabloD97 <pablo97758@gmail.com>
2021-05-07 22:59:12 -0300

    feat: - Se añadio un layout principal basico, con el tiempo se ira mejorando

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,13 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (request,response)=>{ // request - lo que enviamos : res - lo que express nos responde
-    const textInicio = 'Este es el inicio';
-        
-    response.render('inicio',{
-        textInicio
-    })
+router.get('/', (request,response)=>{ // request - lo que enviamos : res - lo que express nos responde        
+    response.render('inicio')
 });
 
 router.get('/nosotros', (request,response)=>{ 
-    const viajes = 'Viaje a Alemania';
 
-    response.render('nosotros', {
-	textoViajes: viajes
-	});
+    response.render('nosotros');
 });
 
 
