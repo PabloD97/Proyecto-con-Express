012f2754b07d3d6efc3283567b9b83ac14495eff
PabloD97 <pablo97758@gmail.com>
2021-05-10 00:03:04 -0300

    feat: - Se agregaron las demas vistas del nav

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,30 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (request,response)=>{ // request - lo que enviamos : res - lo que express nos responde        
-    response.render('inicio')
+    response.render('inicio',{
+        pagina: 'Inicio'
+    });
 });
 
 router.get('/nosotros', (request,response)=>{ 
 
-    response.render('nosotros');
+    response.render('nosotros',{
+        pagina: 'Nosotros'
+    });
+});
+
+router.get('/viajes', (request,response)=>{ 
+
+    response.render('viajes',{
+        pagina: 'Viajes'
+    });
+});
+
+router.get('/testimoniales', (request,response)=>{ 
+
+    response.render('testimoniales',{
+        pagina: 'Testimoniales'
+    });
 });
 
 
