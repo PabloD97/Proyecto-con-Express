aa7563410a73c320048be86743d4d039f6f311bf
PabloD97 <pablo97758@gmail.com>
2021-05-12 16:11:33 -0300

    feat: Routes
    - Ahora el router realiza las peticiones junto con el controller

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,15 @@
 import express from 'express';
-import {paginaInicio} from '../controllers/paginasController.js'
+import {paginaInicio,paginaNosotros,paginaViajes,paginaTestimoniales} from '../controllers/paginasController.js'
 
 const router = express.Router();
 
 router.get('/', paginaInicio);
 
-router.get('/nosotros', (request,response)=>{ 
+router.get('/nosotros', paginaNosotros);
 
-    response.render('nosotros',{
-        pagina: 'Nosotros'
-    });
-});
+router.get('/viajes', paginaViajes);
 
-router.get('/viajes', (request,response)=>{ 
-
-    response.render('viajes',{
-        pagina: 'Viajes'
-    });
-});
-
-router.get('/testimoniales', (request,response)=>{ 
-
-    response.render('testimoniales',{
-        pagina: 'Testimoniales'
-    });
-});
+router.get('/testimoniales', paginaTestimoniales);
 
 
 
