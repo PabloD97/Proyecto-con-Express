ea6bbf0d9cc0734cc8ffea25473daed8dd695e14
PabloD97 <pablo97758@gmail.com>
2021-05-16 00:14:15 -0300

    feat: Controller Testimonial
    -  Ahora cuando el usuario completa los datos del formulario correctamente, este se guarda en la DB

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,5 +1,6 @@
+import {Comentario} from '../models/Comentario.js'
 
-const guardarComentario = (request, response) => {
+const guardarComentario = async (request, response) => {
 
     // Validar 
 
@@ -16,7 +17,27 @@ const guardarComentario = (request, response) => {
         errores.push({mensaje: 'El mensaje esta vacio'})
     }
 
-    console.log(errores)
+    if(errores.length > 0){
+        //Con que alla un error, vamos a mostrar los errores
+        response.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje
+        })
+    } else {
+        //Almacenar en la db
+        try {
+            await Comentario.create({
+                nombre,correo,mensaje
+            })
+
+            response.redirect('/testimoniales');
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 export {
